4f15511568ab8d67ea5d6a798ba65351e1b1717a
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-05 20:47:17 -0300

    Criando Componentes através da Angular CLI

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HelloComponent} from './hello/hello.component';
+import { CursoAngularComponent } from './curso-angular/curso-angular.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    CursoAngularComponent
   ],
   imports: [
     BrowserModule,
---
+++ clientes-app/src/app/curso-angular/curso-angular.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso-angular',
+  templateUrl: './curso-angular.component.html',
+  styleUrls: ['./curso-angular.component.css']
+})
+export class CursoAngularComponent  {
+
+
+
+}
