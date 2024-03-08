348d1fa5c275b1d267e18736a887831d44755415
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-08 17:20:45 -0300

    Conhecendo o Módulo HTTP do Angular

--- clientes-app/src/app/clientes.service.ts
+++ clientes-app/src/app/clientes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 import { Cliente } from './clientes/cliente';
 
 @Injectable({
@@ -6,7 +7,7 @@ import { Cliente } from './clientes/cliente';
 })
 export class ClientesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getCliente(): Cliente{
     let  cliente: Cliente = new Cliente()
