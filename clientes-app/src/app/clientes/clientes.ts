b6de84c318e840792292f5df573cd90d5e4cbccb
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-08 12:19:45 -0300

    Criação de  classe com a data padrão

--- clientes-app/src/app/clientes/clientes.ts
+++ clientes-app/src/app/clientes/clientes.ts
@@ -3,4 +3,10 @@ export class Clientes{
     nome:string;
     cpf:string;
     dataCadastro:string;
+
+    constructor(nome: string, cpf: string) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataCadastro = new Date().toLocaleDateString(); 
+    }
 }
