fb46bd065f79b9174102794e679eea3d17a76bc8
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-13 10:32:31 -0300

    Finalização da arquitetura básica da api

---
+++ api-dot-net/AngularWithApiDotNet/AngularWithApiDotNet.Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace AngularWithApiDotNet.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public long CPF {get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
